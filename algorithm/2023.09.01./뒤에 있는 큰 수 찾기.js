644f26f5851a86555fc0e5c21e99a512ca90c38a
bigperson <daein7076@gmail.com>
2023-09-10 19:00:54 +0900

    뒤에 있는 큰수 찾기, 택배상자

---
+++ algorithm/2023.09.01./뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,20 @@
+// 시간 초과 82.6 / 100
+// 1. numbers 배열을 뒤에서 부터 탐색한다.  변수 최대값과 인덱스를 저장
+// 2. 최대값보다 자신이 크면 최대값을 자신으로 바꾼다.
+function solution(numbers) {
+    const solution = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] === 1000000 || i === numbers.length - 1) {
+            solution.push(-1);
+            continue;
+        }
+        for (let j = i; j < numbers.length; j++) {
+            if (numbers[i] < numbers[j]) {
+                solution.push(numbers[j]);
+                break;
+            }
+            if (j === numbers.length - 1) solution.push(-1);
+        }
+    }
+    return solution;
+}
--- algorithm/2023.09.01./택배 상자.js
+++ algorithm/2023.09.01./택배 상자.js
@@ -1,13 +1,21 @@
-var x = 1;
+function solution(order) {
+    let answer = 0;
+    let stack = 0;
 
-function foo() {
-    var x = 10;
-    bar();
+    for (let i = 0; i < order.length; i++) {
+        if (i + 1 === order.length) {
+            return order.length;
+        }
+        if (order[i] - order[i + 1] === 1) {
+            stack = order[i];
+            continue;
+        }
+        if (stack + 1 === order[i + 1]) {
+            stack = order[i + 1];
+            continue;
+        }
+        answer = i + 1;
+        break;
+    }
+    return answer;
 }
-
-function bar() {
-    console.log(x);
-}
-
-foo();
-bar();
