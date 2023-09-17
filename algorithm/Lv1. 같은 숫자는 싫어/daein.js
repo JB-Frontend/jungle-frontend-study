531e9978af1446aeb8474c1623ad4191dc2ad847
bigperson <daein7076@gmail.com>
2023-09-17 23:11:27 +0900

    ?

--- algorithm/Lv1. 같은 숫자는 싫어/daein.js
+++ algorithm/Lv1. 같은 숫자는 싫어/daein.js
@@ -1,10 +1,10 @@
 function solution(arr) {
-    let prev;
-    let answer = arr.filter((index) => {
-        if (index !== prev) {
-            prev = index;
-            return true;
-        }
-    });
-    return answer;
+  let prev;
+  let answer = arr.filter((index) => {
+    if (index !== prev) {
+      prev = index;
+      return true;
+    }
+  });
+  return answer;
 }
